f0292d1a9bd27a8eded864963d972d63c584e183
Pragya <pragyaaa620@gmail.com>
2024-08-17 14:24:02 +0530

    datatypes

---
+++ basics/02basics.js
@@ -0,0 +1,45 @@
+//Coneversion Operations
+let score="123abc";
+console.log(typeof score);
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// NaN-> Not a Number
+/*
+in boolean: ""->false
+"pragya"-> true
+*/
+// ******************Operations************
+console.log("OPERATIONS");
+
+/*
++,*,-,/,**,%
+++ & --
+*/
+// <, >, >=, <=, !=
+console.log(null>0);
+console.log(null ==0);
+console.log(null>=0);
+// comparisons convert null to a number, treating it as 0
+// thats why null>=0 is true and null>0 is false
+// "===" => strict check
+console.log("2"===2);
+
+//*********Data Types********
+ console.log("DATA TYPES");
+ // Primitive Data Types
+ // 1. Number
+ // 2. String
+ // 3. Boolean
+ // 4. Null
+ // 5. Undefined
+ // 6. Symbol (ES6)
+ // 7. BigInt (ES2020)
+ // Reference (non-primitive)
+ // 1. Array
+ // 2. Object
+ // 3. Functions
+ //stack(primitive) . heap(non-primitive)
+ 
+
