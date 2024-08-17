fcdd3284ecd47233c0f56589019782ebf0f57e27
Pragya <pragyaaa620@gmail.com>
2024-08-17 11:41:07 +0530

    datatypes

---
+++ basics/01basics.js
@@ -0,0 +1,20 @@
+//let, char & let
+const accountId = 1333445;
+// var name = "Pragya";
+let password = "12345";
+let email = "pragya@google.com";
+console.table([accountId, password, email]);
+/* prefer not to use var 
+// because of issue in block scope and functional scope*/
+
+//Datatypes and ECMA standards
+("use strict"); //treat all JS code as newer version
+// number-> 2 to power 53
+// bigInt
+// string-> " "
+// boolean-> true/false
+//null-> standalone value
+// undefined-> value has not been assigned
+// symbol-> uniqueness
+//object
+console.log(typeof email);
